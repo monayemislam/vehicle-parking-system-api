21dd80274f5735d8fe041443396102b518df17f9
Monayem Islam <monayem.dev@gmail.com>
2022-06-27 17:33:35 +0600

    Middleware for admin and manager is deleted

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -62,8 +62,6 @@ class Kernel extends HttpKernel
         'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'role'=> \Illuminate\Http\Middleware\isAdmin::class,
-        'role'=> \Illuminate\Http\Middleware\isManager::class,
+        'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class
     ];
 }
